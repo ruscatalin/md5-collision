9065fb51e46928a5af0b5b9f7c5b77586dba2b7b
ruscatalin <ruscatalin97@gmail.com>
2022-03-07 17:36:09 +0100

    added retro music. created media folder

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QTextEdit, QDialog
+from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
+from PyQt5.QtCore import QUrl
 import sys
 import os
 from md5 import MD5
@@ -16,7 +18,8 @@ class MD5cracker(QDialog):
 app = QApplication(sys.argv)
 window = MD5cracker()
 uic.loadUi(os.path.join(sys.path[0], "gui.ui"), window)
-window.setWindowIcon(QIcon(os.path.join(sys.path[0], "md5.png")))
+window.setWindowIcon(QIcon(os.path.join(sys.path[0], "media/md5.png")))
+player = QMediaPlayer()
 
 input = window.findChild(QTextEdit, 'input')
 button = window.findChild(QPushButton, 'pushButton')
@@ -31,8 +34,16 @@ def click():
     update_output(MD5.hash(input.toPlainText()))
     # update_output("Clicked!")
 
+def play_audio():
+    url = QUrl.fromLocalFile(os.path.join(sys.path[0], "media/Jester-Elysium.mp3"))
+    song = QMediaContent(url)
+    player.setMedia(song)
+    player.setVolume(35)
+    player.play()
+
 button.clicked.connect(click)
 
 window.show()
+play_audio()
 app.exec_()
 
