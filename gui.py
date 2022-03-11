9117314a2bfa220869bc8ed4cf5d025bc644a202
ruscatalin <ruscatalin97@gmail.com>
2022-03-11 19:38:08 +0100

    Added volume control to GUI

--- gui.py
+++ gui.py
@@ -1,11 +1,14 @@
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap, QIcon
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QTextEdit, QDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QTextEdit, QDialog, QSlider
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtCore import QUrl
 import sys
 import os
 from md5 import MD5
+
+
+VOLUME = 3
 
 
 class MD5cracker(QDialog):
@@ -25,6 +28,9 @@ input = window.findChild(QTextEdit, 'input')
 button = window.findChild(QPushButton, 'pushButton')
 output = window.findChild(QLabel, 'output')
 
+volume_icon = window.findChild(QLabel, 'volumeIcon')
+volume_slider = window.findChild(QSlider, 'volumeSlider')
+
 
 def update_output(text):
     output.setText(text)
@@ -34,14 +40,28 @@ def click():
     update_output(MD5.hash(input.toPlainText()))
     # update_output("Clicked!")
 
+def update_volume(value):
+    VOLUME = value
+    player.setVolume(VOLUME)
+    if value == 0:
+        volume_icon.setPixmap(QPixmap(os.path.join(sys.path[0], "media/volume_0.png")))
+    elif value <= 33:
+        volume_icon.setPixmap(QPixmap(os.path.join(sys.path[0], "media/volume_low.png")))
+    else:
+        volume_icon.setPixmap(QPixmap(os.path.join(sys.path[0], "media/volume_medium.png")))
+
+def slide():
+    update_volume(int(volume_slider.value()))
+
 def play_audio():
     url = QUrl.fromLocalFile(os.path.join(sys.path[0], "media/Jester-Elysium.mp3"))
     song = QMediaContent(url)
     player.setMedia(song)
-    player.setVolume(35)
+    update_volume(VOLUME)
     player.play()
 
 button.clicked.connect(click)
+volume_slider.valueChanged.connect(slide)
 
 window.show()
 play_audio()
